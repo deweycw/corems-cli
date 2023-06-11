65701c20c9c24a10c9921d911dbf3013a38481d6
Christian Dewey <dewey.christian@gmail.com>
2023-06-10 21:10:22 -0700

    attempting windows build

--- install_MacOS.sh
+++ install_MacOS.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
+docker "container ls"
 mkdir -p "${HOME}/lcms-asgn/bin"
 cd "$(dirname "$0")"
-ls .
 cp "./docker-rust" ${HOME}/lcms-asgn/bin/
 chmod 755 "${HOME}/lcms-asgn/bin/docker-rust"
-
 echo "export PATH=${HOME}/lcms-asgn/bin:${PATH}" >> ~/.bash_profile
 
